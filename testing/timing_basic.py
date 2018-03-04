2c0b031d3435772193890fa7269f033dc6d6e755
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-03-04 17:28:56 -0500

    Create timing_basic.py

---
+++ testing/timing_basic.py
@@ -0,0 +1,25 @@
+"""
+
+A basic resource for measuring the time-performance of code using the timeit module
+
+To use timeit you create a Timer object whose parameters are two Python statements. 
+The first parameter is a Python statement that you want to time; 
+the second parameter is a statement that will run once to set up the test. 
+The timeit module will then time how long it takes to execute the statement some number of times. 
+
+By default timeit will try to run the statement one million times. 
+When its done it returns the time as a floating point value representing the total number of seconds. 
+
+"""
+
+from timeit import Timer
+
+# Define a function to be timed
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+        
+#declare a timer
+t1 = Timer("test1()", "from __main__ import test1")
+print("concat ",t1.timeit(number=1000), "milliseconds")
