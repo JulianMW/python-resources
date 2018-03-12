7fe96f37ab0f7f2c1224a4685c3083bc766cce27
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-03-12 08:40:37 -0400

    Update binary_search_range.py

--- search-algorithms/binary_search_range.py
+++ search-algorithms/binary_search_range.py
@@ -5,42 +5,64 @@ Count the number of appearances of a value in a list using binary search
 """
 
 def binary_search_range(array, target):
-    max_index = len(array)
+    array_max_index = len(array) -1
     lower = 0
-    upper = max_index
+    upper = array_max_index
     
     min_index = False
     max_index = False
     
+    print("finding min index")
+    
     # find lower range
     while lower < upper:
         x = lower + (upper - lower) // 2
+        print(x)
         val = array[x]
         if target == val:
             if ( x > 0 and val > array[x-1] ) or ( x == 0 ):
                 min_index = x
                 break
+            else:
+                upper = x
+        elif target > val:
+            if lower == x:
+                break
+            lower = x
+        elif ( target < val):
+            upper = x
+            
+    print('min index is', min_index,'!')
+    
+    print("finding max index")
+    
+    # find upper range
+    # reset the max value and begin search again with same lower value
+    upper = array_max_index
+    while lower < upper:
+        x = lower + (upper - lower) // 2
+        print(x)
+        val = array[x]
+        if target == val:
+            if ( x < array_max_index and val < array[x+1] ) or ( x == array_max_index ):
+                max_index = x
+                break
+            else:
+                lower = x
         elif target > val:
             if lower == x:
                 break
             lower = x
         elif target < val:
             upper = x
-    
-    
-    # find upper range
-    # reset the max value and begin search again with same lower value
-    upper = max_index
-    while lower < upper:
-        x = lower + (upper - lower) // 2
-        val = array[x]
-        if target == val:
-            return x
-        elif target > val:
-            if lower == x:
-                if ( x > 0 and val > array[x-1] ) or ( x == 0 ):
-                    min_index = x
-                    break
-            lower = x
-        elif target < val:
-            upper = x
+            
+    print('max index is', max_index,'!')
+        
+        
+    if min_index and not max_index:
+        max_index == min_index
+        return (max_index - min_index + 1)
+    elif min_index and max_index:
+        return (max_index - min_index + 1)
+    else:
+        return False
