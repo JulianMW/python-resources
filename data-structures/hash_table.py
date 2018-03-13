afef39a3ec8743b6d6197aa819e5f2020525d919
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-03-13 14:55:41 -0400

    Update hash_table.py

--- data-structures/hash_table.py
+++ data-structures/hash_table.py
@@ -1,3 +1,31 @@
 
 
 class HashTable(length):
+    def __init__(self):
+        self.data = [None]*length
+        self.keys = [None]*length
+        self.length = length
+        
+    def put(self,key,value):
+        hashval = hash_function(key)
+        if self.keys[hashval] == None:
+            self.keys[hashval] = key
+            self.data[hashval] = value
+        else:
+            # if there is data there already, and the key matches
+            if self.keys[hashval] == key:
+                self.data[hashval] = value
+            # if there is no data there and the key doesn't match
+            elif self.keys[hashval] != key:
+                while self.keys[hashval] != key and self.keys[hashval] != None: # consider including case for full table
+                    hashval = rehash_function(hashval)
+                self.keys[hashval] = key
+                self.data[hashval] = value
+                
+                
+    def hash_function(self,key):
+        return (key%self.length)
+    
+    def rehash_function(self,oldhash):
+        return (oldhash+1)%self.size
+            
