47ab876bb3e10a6836d20b1d7ea5ec85c1f49a23
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-04-01 14:03:12 -0400

    added recursive implementation

--- search-algorithms/binary_search.py
+++ search-algorithms/binary_search.py
@@ -1,5 +1,29 @@
+# Recursive implementation
+def recursive_binary_search(input_list, l, r, target):
+    if not input_list:
+        raise RuntimeError("List is empty")
+    
+    if l <= r:
+        
+        midpoint = l + (r - l)//2
+        
+        if input_list[midpoint] == target:
+            return midpoint
+        
+        if input_list[midpoint] < target:
+            print('a ', str(midpoint), ' l-',str(l),' r-',str(r))
+            return recursive_binary_search(input_list, midpoint + 1, r, target)
+        elif input_list[midpoint] > target:
+            print('b ', str(midpoint), ' l-',str(l),' r-',str(r))
+            return recursive_binary_search(input_list, l, midpoint - 1, target)
+        
+    else:
+        return -1
+    
+print(recursive_binary_search(to_sort, 0, len(to_sort) - 1, 8))
 
 
+# Loop implementation
 def BinarySearch(array, target):
     lower_index = 0
     upper_index = len(array) - 1
