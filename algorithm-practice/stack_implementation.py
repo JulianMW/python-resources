74a1338b065050040742f090d8a80407b807a99a
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-04-04 12:35:28 -0400

    Only contains a sketch of the solution

--- algorithm-practice/stack_implementation.py
+++ algorithm-practice/stack_implementation.py
@@ -1,19 +1,33 @@
 
 class SpecialStack():
+    
+    __init__(self, arr, n, capacity):
+        self.min_element = None
+        self.max_capacity = capacity
+        self.size = 0
+        self.arr = []
+        self.current_mins = [] # if you want to track the minimum element
+        
+        if n > self.capacity:
+            raise IndexError("your array has more elements than this stack's capacity")
+        else:
+            for x in arr:
+                push(arr, x)
+        
 
-# Your task is to complete all these function's
-# function should append an element on to the stack
-def push(arr, ele):
-    # Code here
-# Function should pop an element from stack
-def pop(arr):
-    # Code here
-# function should return 1/0 or True/False
-def isFull(n, arr):
-    # Code here
-# function should return 1/0 or True/False
-def isEmpty(arr):
-    #Code here
-# function should return minimum element from the stack
-def getMin(n, arr):
-    # Code here
+    # Your task is to complete all these function's
+    # function should append an element on to the stack
+    def push(arr, ele):
+        if isFull
+    # Function should pop an element from stack
+    def pop(arr):
+        # Code here
+    # function should return 1/0 or True/False
+    def isFull(n, arr):
+        # Code here
+    # function should return 1/0 or True/False
+    def isEmpty(arr):
+        #Code here
+    # function should return minimum element from the stack
+    def getMin(n, arr):
+        # Code here
