16313d6735c93a59f346f8019ab584afab9e3b9d
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-04-04 12:17:50 -0400

    Create stack_implementation.py

---
+++ algorithm-practice/stack_implementation.py
@@ -0,0 +1,19 @@
+
+class SpecialStack():
+
+# Your task is to complete all these function's
+# function should append an element on to the stack
+def push(arr, ele):
+    # Code here
+# Function should pop an element from stack
+def pop(arr):
+    # Code here
+# function should return 1/0 or True/False
+def isFull(n, arr):
+    # Code here
+# function should return 1/0 or True/False
+def isEmpty(arr):
+    #Code here
+# function should return minimum element from the stack
+def getMin(n, arr):
+    # Code here
